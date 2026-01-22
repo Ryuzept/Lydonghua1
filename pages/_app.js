6b1d02d0ea9cab1e10a450c6c7623c0e4693d878
Ryuzept <dannd5119@gmail.com>
2026-01-23 01:20:56 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,4 @@
 import '../styles/globals.css'
-import { ThemeProvider } from '../context/ThemeContext'
-
 export default function App({ Component, pageProps }) {
-  return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
+  return <Component {...pageProps} />
 }
