177caeafd9794035651965f00f3ada2a6734d624
Ryuzept <dannd5119@gmail.com>
2026-01-23 00:38:12 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
   ],
   theme: {
     extend: {
       colors: {
         gold: "#d4af37",
         golddark: "#b8962e",
-        dark: "#0f0f0f"
-      }
-    }
+        dark: "#0f0f0f",
+      },
+    },
   },
   plugins: [],
 }
